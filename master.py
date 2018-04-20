e8eac62724c125861ed7f4da8d0e9a6d74137f7c
unknown <s1691@wowshga.internal>
2018-04-20 16:28:44 +0800

    version-2.0

--- master.py
+++ master.py
@@ -11,8 +11,9 @@ import os
 import sys
 import time
 
+
 def view_directory(filename=""):
-    result = os.popen('ls /home/%s' %filename).readlines()
+    result = os.popen('ls /home/%s' % filename).readlines()
     return result
 
 
@@ -22,71 +23,60 @@ def group(path='/etc/salt/master'):
 
 
 def get_group(path='/etc/salt/master.d/group.conf'):
-    f=open(path)
-    group=f.readlines()
+    f = open(path)
+    group = f.readlines()
     f.close()
     return group[1:]
 
-def update_group(gruop,newip,path='/etc/salt/master.d/group.conf'):
-    bakpath=path+'.bak'
-    f=open(path)
-    substance=f.readlines()
+
+def update_group(gruop, newip, path='/etc/salt/master.d/group.conf'):
+    bakpath = path + '.bak'
+    f = open(path)
+    substance = f.readlines()
     f.close()
     if os.path.isfile(bakpath):
         os.remove(bakpath)
-    os.rename(path,bakpath)
+    os.rename(path, bakpath)
     for i in substance:
-        if i.find(gruop+":") >=0:
-            oldgruop=i
-            c=i.split("'")
-            c.insert(len(c)-1,newip)
-            d=",".join(c[1:-1])
-            newgroup="'".join([c[0],d,c[-1]])
-            substance[substance.index(oldgruop)]=newgroup
-            f=open(path,"w")
+        if i.find(gruop + ":") >= 0:
+            oldgruop = i
+            c = i.split("'")
+            c.insert(len(c) - 1, newip)
+            d = ",".join(c[1:-1])
+            newgroup = "'".join([c[0], d, c[-1]])
+            substance[substance.index(oldgruop)] = newgroup
+            f = open(path, "w")
             f.writelines(substance)
             f.close()
 
-def buildproject(project,buildtype,buildcommand,svnurl):
-#    svnurl = 'https://192.168.112.237/svn/pro/'
-    username = 'otbuild'
-    password = 'wowgm#23'
+
+def buildproject(project, buildtype, buildcommand, svnurl):
+    username = 'admin'
+    password = '123456'
     svnbase = '/home/data/svnbase/'
     saltbase = '/home/data/salt/upload'
-    
+
     projecturl = svnurl
     project = project
     buildtype = buildtype
     buildcommand = buildcommand
     projectfile = svnbase + project
-    if  not os.path.exists(projectfile):
+    if not os.path.exists(projectfile):
         os.mkdir(projectfile)
-#    if not version:
-#        if project == "dsync-heroes":
-#            projecturl = svnurl + 'trunk/' + "apollo/" + project
-#        else:
-#            projecturl = svnurl + 'trunk/' + project
-#    else:
-#        projecturl = svnurl + project + '/releases/' + version
-    svnresult = os.system('cd %s && svn co  --username %s --password %s %s' %(projectfile,username,password,projecturl))
-    result={}
+    svnresult = os.system('cd %s && svn co  --username %s --password %s %s' % (projectfile, username, password, projecturl))
+    result = {}
     if svnresult == 0:
         projectpath = svnbase + project + '/' + svnurl.split('/')[-1]
-        mvnresult = os.system('cd %s  && %s' %(projectpath,buildcommand) )
+        mvnresult = os.system('cd %s  && %s' % (projectpath, buildcommand))
         if buildtype == 'war':
-            mvto = os.system('mv %s/target/%s.war %s/%s.war' %(projectpath,project,saltbase,project) )
+            mvto = os.system('mv %s/target/%s.war %s/%s.war' % (projectpath, project, saltbase, project))
         elif buildtype == 'jar':
-            mvto = os.system('mv %s/target/%s.jar %s/%s.jar' %(projectpath,project,saltbase,project) )
+            mvto = os.system('mv %s/target/%s.jar %s/%s.jar' % (projectpath, project, saltbase, project))
         if mvto == 0:
-	    result['master']='project building successful'
+            result['master'] = 'project building successful'
         else:
-	    result['master']='project building failed'
+            result['master'] = 'project building failed'
         return result
     else:
-        result['master']='svn checkout failed'
+        result['master'] = 'svn checkout failed'
         return result
-
-
-if __name__ == "__main__":
-#    view_directory()
-    buildproject('war3redeem','jar','mvn package -DskipTests','https://192.168.112.237/svn/pro/app-war3-redeem/releases/v1.0')
